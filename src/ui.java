fe0cc26f3af2dc7c2644fbd34f621da6a9a87b21
arisia <starlights2283@gmail.com>
2023-09-21 00:16:40 +0900

    update

--- src/ui.java
+++ src/ui.java
@@ -5,8 +5,22 @@ import java.util.Scanner;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class ui {
+    public static boolean isValidEmail(String email) {
+        boolean err = false;
+        String regex = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(email);
+        if(m.matches()) {
+            err = true;
+        }
+        return err;
+    }
+
     public static void main(String[] args) throws IOException {
         boolean login = false;
         boolean exit = false;
@@ -30,6 +44,13 @@ public class ui {
                         System.out.println("--------------로그인--------------");
                         System.out.println("email을 입력해주세요!");
                         String user_email = sc.next();
+                        if(isValidEmail(user_email)){
+                            System.out.println("이메일 유효성 검사 통과!");
+                        }else {
+                            System.out.println("올바르지 않은 이메일 형식입니다.");
+                            System.out.println("다시 시도해주세요!");
+                            break;
+                        }
                         String umail = null;
                         String upassword = null;
                         try {
@@ -61,6 +82,13 @@ public class ui {
                         System.out.println("계정 정보 찾기");
                         System.out.println("회원가입에 사용한 email을 입력해주세요!");
                         String find_email = sc.next();
+                        if(isValidEmail(find_email)){
+                            System.out.println("이메일 유효성 검사 통과!");
+                        }else {
+                            System.out.println("올바르지 않은 이메일 형식입니다.");
+                            System.out.println("다시 시도해주세요!");
+                            break;
+                        }
                         if (find_email.equals(email)) {
                             System.out.println("이메일을 찾았습니다! 변경할 비밀번호를 입력해주세요!");
                             String new_password;
@@ -83,6 +111,13 @@ public class ui {
                         System.out.println("--------------회원가입--------------");
                         System.out.println("email을 입력해주세요!");
                         String new_email = sc.next();
+                        if(isValidEmail(new_email)){
+                            System.out.println("이메일 유효성 검사 통과!");
+                        }else {
+                            System.out.println("올바르지 않은 이메일 형식입니다.");
+                            System.out.println("다시 시도해주세요!");
+                            break;
+                        }
                         System.out.println("password를 입력해주세요!");
                         String new_password = sc.next();
                         String user_name = new_email.split("@")[0];
